a87e2ca6600216f1715ace1f66afa32a0de3a3c9
Shubhra Sinha <shubhrasinha874@gmail.com>
2022-01-19 16:31:27 +0530

    pong game added

---
+++ pong_game.py
@@ -0,0 +1,98 @@
+import turtle as t
+playerAscore=0
+playerBscore=0
+
+window= t.Screen()
+window.title("Pong Game")
+window.bgcolor("green")
+window.setup(width=800, height=600)
+window.tracer(0)
+
+#creating left paddle
+
+leftpaddle=t.Turtle()
+leftpaddle.speed(0)
+leftpaddle.shape("square")
+leftpaddle.color("white")
+leftpaddle.shapesize(stretch_wid=5,stretch_len=1)
+leftpaddle.penup()
+leftpaddle.goto(-350,0)
+
+#creating right paddle
+
+rightpaddle=t.Turtle()
+rightpaddle.speed(0)
+rightpaddle.shape("square")
+rightpaddle.color("white")
+rightpaddle.shapesize(stretch_wid=5,stretch_len=1)
+rightpaddle.penup()
+rightpaddle.goto(350,0)
+
+#creating ball
+
+ball=t.Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("red")
+ball.penup()
+ball.goto(5,5)
+ballxdirection=0.2
+ballydirection=0.2
+
+#creating pen for scorecard update
+
+pen=t.Turtle()
+pen.speed(0)
+pen.color("Blue")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("score", align="center", font=('Arial',24,'normal'))
+
+#moving the left paddle
+
+def leftpaddleup():
+    y=leftpaddle.ycor()
+    y=y+90
+    leftpaddle.sety(y)
+
+def leftpaddledown():
+    y=leftpaddle.ycor()
+    y=y-90
+    leftpaddle.sety(y)
+
+#moving the right paddle
+
+def rightpaddleup():
+    y=rightpaddle.ycor()
+    y=y+90
+    rightpaddle.sety(y)
+
+def rightpaddledown():
+    y=rightpaddle.ycor()
+    y=y-90
+    rightpaddle.sety(y)
+
+#Assign keys to play
+
+window.listen()
+window.onkeypress(leftpaddleup,'w')
+window.onkeypress(leftpaddledown,'s')
+window.onkeypress(rightpaddleup,'Up')
+window.onkeypress(rightpaddledown,'Down')
+
+while True:
+    window.update()
+
+    #moving the ball
+
+
+
+
+
+
+
+
+
+
+
